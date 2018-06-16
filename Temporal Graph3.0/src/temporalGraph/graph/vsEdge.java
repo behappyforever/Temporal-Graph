e377a603c814fcb689731b2d600a1a141274450d
behappyforever <1026007963@qq.com>
2018-06-16 17:22:43 +0800

    完成存储功能

--- Temporal Graph3.0/src/temporalGraph/graph/vsEdge.java
+++ Temporal Graph3.0/src/temporalGraph/graph/vsEdge.java
@@ -1,12 +1,25 @@
 package temporalGraph.graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class vsEdge {
     private long desId;
-    private long[] weight;//保存VS中带时间戳的权值
+//    private long[] weight;//保存VS中带时间戳的权值,未压缩前
+    private List<Long> weightDisplay;//权值表
+    private byte[] weightIndex;//对应到权值表的索引
 
-    public vsEdge(long id, long[] w){
+
+    public vsEdge(long id, long[] weightArr){
         desId=id;
-        weight=w;
+        weightDisplay=new ArrayList();
+        weightIndex=new byte[TGraph.timeRange];
+        for (int i = 0; i < weightArr.length; i++) {
+            if(!weightDisplay.contains(weightArr[i])){//权值表不能有重复元素，不能用set
+                weightDisplay.add(weightArr[i]);
+            }
+            weightIndex[i]=(byte) weightDisplay.indexOf(weightArr[i]);
+        }
     }
 
     public long getDesId() {
